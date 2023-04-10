cefb439f87860511ef2603d35c6cf7384e2c03c8
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2023-04-10 19:03:32 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react-do-it
+
+Proyecto creado por Rodrigo Vergara utilizando React
+
+Una lista de tareas por hacer.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './scss/import.scss';
+import MainContainer from "./containers/MainContainer";
+import Footer from "./components/Footer";
+
+function App() {
+  return <>
+    <MainContainer />
+    <Footer />
+  </>;
+}
+
+export default App;
