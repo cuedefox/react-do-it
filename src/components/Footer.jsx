2fc24b0de95b8e52f2be9294040c3b6b6b974855
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2023-04-10 20:09:01 -0300

    preparo estructura

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Footer = () => {
-    return <>
+    return <footer>
     
-    </>
+    </footer>
 }
 
 export default Footer;
--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const MainContainer = () => {
-    return <>
+    return <main>
     
-    </>
+    </main>
 }
 
 export default MainContainer;
