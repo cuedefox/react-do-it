f3a8b5e387cba6f065e1a588321ec0b08e305a16
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2023-04-11 15:55:16 -0300

    agrego scss

--- src/Items/Item.jsx
+++ src/Items/Item.jsx
@@ -3,7 +3,7 @@ import {React} from "react";
 const Item = ({item, deleteItem, checkItem}) => {
     return <div className="item">
         <input className="check-item" type="checkbox" onClick={() => checkItem(item.id)} />
-        <p>{item.title}</p>
+        <p>{item.check ? <span>{item.title}</span> : item.title}</p>
         <input className="delete-item" type="button" onClick={() => deleteItem(item.id)} />
     </div>
 }
--- src/contexts/Items.jsx
+++ src/contexts/Items.jsx
@@ -3,15 +3,21 @@ import { createContext, useState, React} from "react";
 export const Items = createContext({});
 
 const ItemsProvider = ({children}) => {
-    const [items, setItems] = useState([]);
+    
+    const [items, setItems] = useState(() => {
+        const storedTareas = localStorage.getItem('items');
+        return storedTareas ? JSON.parse(storedTareas) : [];
+      });;
 
     const addItem = (itemToAdd) => {
         setItems([...items, itemToAdd]);
+        localStorage.setItem('items', JSON.stringify(items));
     }
 
     const deleteItem = (id) => {
         const itemsWithoutDeleted = items.filter(item => item.id !== id);
         setItems(itemsWithoutDeleted);
+        localStorage.setItem('items', JSON.stringify(items));
     }
 
     const checkItem = (id) => {
@@ -19,10 +25,12 @@ const ItemsProvider = ({children}) => {
         const index = itemsAndChecked.findIndex((item) => item.id === id);
         itemsAndChecked[index].check = !itemsAndChecked[index].check;
         setItems(itemsAndChecked);
+        localStorage.setItem('items', JSON.stringify(items));
     }
 
     const deleteAllItems = () => {
         setItems([]);
+        localStorage.setItem('items', JSON.stringify(items));
     }
 
     return <Items.Provider
